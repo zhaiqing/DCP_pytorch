e8dbb9474bd510b3e7e159734efd8bc2f45c3531
zhaiqing <33917900+zhaiqing@users.noreply.github.com>
2021-07-18 16:35:01 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,356 @@
+
+import os
+import sys
+import copy
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from util import quat2mat
+
+def clones(module, N):
+    """module深复制N次"""
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+def attention(query, key, value, mask=None, dropout=None):
+    """
+    注意力计算
+    query、key、value的形状类似于(nbatches, self.h, num_points, self.d_k)
+    mask:
+    """
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1).contiguous()) / math.sqrt(d_k)
+    if mask is not None:
+        # masked_fill_(mask, value):用value填充tensor中与mask中值为1位置相对应的元素。mask的形状必须与要填充的tensor形状一致
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim=-1)
+    return torch.matmul(p_attn, value), p_attn
+
+
+def nearest_neighbor(src, dst):
+    inner = -2 * torch.matmul(src.transpose(1, 0).contiguous(), dst)
+    distances = -torch.sum(src**2, dim=0, keepdims=True).transpose(1, 0).contihuous() - inner - torch.sum(dst**2, dim=0, keepdims=True)
+
+    distances, indices = distances.topk(k=1, dim=-1)
+    return distances, indices
+
+
+def knn(x, k):
+    """x: (batch_size, 3, num_points)"""
+    inner = -2 * torch.matmul(x.transpose(2, 1).contiguous(), x)
+    xx = torch.sum(x**2, dim=1, keepdims=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1).contiguous()
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]
+    return idx     # (batch_size, num_points, k)
+
+# 寻找knn，获得feature_edge
+def get_graph_feature(x, k=20):
+    # x = x.squeeze()
+    idx = knn(x, k=k)   # (batch_size, num_points, k)
+    batch_size, num_points, _ = idx.size()
+    device = torch.device('cuda')
+
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points   # (batch_size, 1, 1)
+
+    idx = idx + idx_base       # idx: (batch_size, num_points, k)
+
+    idx = idx.view(-1)      # idx: (batch_size*num_points*k)
+
+    _, num_dims, _ = x.size()    # x: (batch_size, 3, num_points)
+
+    x = x.transpose(2, 1).contiguous()   # x: (batch_size, num_points, 3)
+    feature = x.view(batch_size * num_points, -1)[idx, :]    # (batch_size*num_points*k, 3)
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+
+    feature = torch.cat((feature, x), dim=3).permute(0, 3, 1, 2)
+    # feature: (batch_size, num_dims*2, num_points, k)
+    return feature
+
+
+class EncoderDecoder(nn.Module):
+    """标准的编解码器结构"""
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.generator(self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask))
+
+
+class Generator(nn.Module):
+    def __init__(self, emb_dims):
+        super(Generator, self).__init__()
+        self.nn = nn.Sequential(nn.Linear(emb_dims, emb_dims // 2),
+                                nn.BatchNorm1d(emb_dims // 2),
+                                nn.ReLU(),
+                                nn.Linear(emb_dims // 2, emb_dims // 4),
+                                nn.BatchNorm1d(emb_dims // 4),
+                                nn.ReLU(),
+                                nn.Linear(emb_dims // 4, emb_dims // 8),
+                                nn.BatchNorm1d(emb_dims // 8),
+                                nn.ReLU())
+        self.proj_rot = nn.Linear(emb_dims // 8, 4)
+        self.proj_trans = nn.Linear(emb_dims // 8, 3)
+
+    def forward(self, x):
+        x = self.nn(x.max(dim=1)[0])
+        rotation = self.proj_rot(x)
+        translation = self.proj_trans(x)
+        rotation = rotation / torch.norm(rotation, p=2, dim=1, keepdim=True)
+        return rotation, translation
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class Encoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class SublayerConnection(nn.Module):
+    """将x通过LayerNorm后，再通过作为参数传入的网络sublayer，最后与x相加，返回"""
+    def __init__(self, size, dropout=None):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+
+    def forward(self, x, sublayer):
+        """sublayer是一个具体的MultiHeadAttention或者PositionwiseFeedForward对象"""
+        return x + sublayer(self.norm(x))
+
+
+class EncoderLayer(nn.Module):
+    """Encoder is made up of self-attn and feed forward (defined below) """
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn   # MultiHeadedAttention
+        self.feed_forward = feed_forward   # PositionwiseFeedForward
+        # 使用深度克隆方法，完整地复制出来两个SublayerConnection
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        """使用了lambda来定义一个匿名函数,然后把这个lambda传递给self.sublayer[0]"""
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % h == 0
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = None
+
+    def forward(self, query, key, value, mask=None):
+        """q、k、v矩阵"""
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2).contiguous()
+                             for l, x in zip(self.linears, (query, key, value))]
+
+        # 2) Apply attention on all the projected vectors in batch.
+        # 计算Self-Attention
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # 3) "Concat" using a view and apply a final linear.
+        # 通过view进行Concatenate，将之应用于Linear网络输出
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.norm = nn.Sequential()
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = None
+
+    def forward(self, x):
+        return self.w_2(self.norm(F.relu(self.w_1(x)).transpose(2, 1).contiguous()).transpose(2, 1).contiguous())
+
+
+class Transformer(nn.Module):
+    """Transformer的实现"""
+    def __init__(self, args):
+        super(Transformer, self).__init__()
+        self.emb_dims = args.emb_dims
+        self.N = args.n_blocks
+        self.dropout = args.dropout
+        self.ff_dims = args.ff_dims    # Feed_forward Dims
+        self.n_heads = args.n_heads    # Multihead Attention的头数
+        c = copy.deepcopy
+        attn = MultiHeadAttention(self.n_heads, self.emb_dims)
+        ff = PositionwiseFeedForward(self.emb_dims, self.ff_dims, self.dropout)
+        self.model = EncoderDecoder(Encoder(EncoderLayer(self.emb_dims, c(attn), c(ff), self.dropout), self.N),
+                                    Decoder(DecoderLayer(self.emb_dims, c(attn), c(ff), self.dropout), self.N),
+                                    nn.Sequential(),
+                                    nn.Sequential(),
+                                    nn.Sequential())
+
+    def forward(self, *input):
+        """input: (batch_size, emb_dims, num_points)"""
+        src = input[0]
+        tgt = input[1]
+        src = src.transpose(2, 1).contiguous()
+        tgt = tgt.transpose(2, 1).contiguous()
+        tgt_embedding = self.model(src, tgt, None, None).transpose(2, 1).contiguous()
+        src_embedding = self.model(tgt, src, None, None).transpose(2, 1).contiguous()
+        return src_embedding, tgt_embedding
+
+
+
+
+class PointNet(nn.Module):
+    def __init__(self, emb_dims=512):
+        super(PointNet, self).__init__()
+        self.conv1 = nn.Conv1d(3, 64, kernel_size=1, bias=False)
+        self.conv2 = nn.Conv1d(64, 64, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv1d(64, 64, kernel_size=1, bias=False)
+        self.conv4 = nn.Conv1d(64, 128, kernel_size=1, bias=False)
+        self.conv5 = nn.Conv1d(128, emb_dims, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.bn4 = nn.BatchNorm1d(128)
+        self.bn5 = nn.BatchNorm1d(emb_dims)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        return x
+
+class DGCNN(nn.Module):
+    def __init__(self, emb_dims=512):
+        self.conv1 = nn.Conv2d(6, 64, kernel_size=1, bias=False)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=1, bias=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=1, bias=False)
+        self.conv5 = nn.Conv2d(256, emb_dims, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(emb_dims)
+
+    def forward(self, x):
+        batch_size, num_dims, num_points = x.size()
+        x = get_graph_feature(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x1 = x.max(dim=-1, keepdims=True)[0]
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x2 = x.max(dim=-1, keepdims=True)[0]
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x3 = x.max(dim=-1, keepdims=True)[0]
+
+        x = F.relu(self.bn4(self.conv4(x)))
+        x4 = x.max(dim=-1, keepdims=True)[0]
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        x = F.relu(self.bn5(self.conv5(x))).view(batch_size, -1, num_points)
+        return x
+
+
+class MLPHead(nn.Module):
+    def __init__(self, args):
+        super(MLPHead, self).__init__()
+        emb_dims = args.emb_dims
+        self.emb_dims = emb_dims
+        self.nn = nn.Sequential(nn.Linear(emb_dims * 2, emb_dims // 2),
+                                nn.BatchNorm1d(emb_dims // 2),
+                                nn.ReLU(),
+                                nn.Linear(emb_dims // 2, emb_dims // 4),
+                                nn.BatchNorm1d(emb_dims // 4),
+                                nn.ReLU(),
+                                nn.Linear(emb_dims // 4, emb_dims //8),
+                                nn.BatchNorm1d(emb_dims // 8),
+                                nn.ReLU())
+        self.proj_rot = nn.Linear(emb_dims // 8, 4)
+        self.proj_trans = nn.Linear(emb_dims // 8, 3)
+
+    def forward(self, *input):
+        src_embedding = input[0]
+        tgt_embedding = input[1]
+        embedding = torch.cat((src_embedding, tgt_embedding), dim=1)
+        embedding = self.nn(embedding.max(dim=-1)[0])
+        rotation = self.proj_rot(embedding)
+        rotation = rotation / torch.norm(rotation, p=2, dim=1, keepdim=True)
+        translation = self.proj_trans(embedding)
+        return quat2mat(rotation), translation  # 返回旋转矩阵和平移矩阵
+
+
+
+
+
